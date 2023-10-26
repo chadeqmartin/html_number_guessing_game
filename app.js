51f97f002a32b39ee81943a739e6e11d8eb65172
chadeqmartin <chadeqmartin@gmail.com>
2023-10-26 13:52:22 -0400

    starter code

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+
+let random = Math.floor(Math.random()*(101- 1)) + 1;
+
+function numberGame(){
+    
+    let userInput = document.getElementById('input-name')
+    // userInput = userInput.value
+    console.log(userInput)
+    let userOutput = document.getElementById('output')
+    
+    if (userInput === random){
+        userOutput.innerText = "Congrats, you won!";
+    } else if (userInput < random){
+        userOutput.innerText = 'Too low';
+        userInput = ""
+    } else {
+        userOutput.innerText = 'Too high';
+        userInput = ""
+    }
+}   
+
